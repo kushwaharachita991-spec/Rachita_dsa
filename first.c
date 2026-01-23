56c5c499f380cb6c74ca2a1eb9ffe6b0c8abc7c5
Rachita Kushwaha <A20405225029@AUJPR.AMITY.EDU.IN>
2026-01-23 14:05:52 +0530

    first commit

---
+++ first.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int getsmax(int arr[], int n)
+{
+    int max =  arr[0];
+    int smax= -1;
+    forint i=1, i<n; i++)
+    {
+        if (arr[i] > max)
+        {
+            smax = max;
+            max = arr[i]
+        }
+    }
+}
